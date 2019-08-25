23627fc76cfffe5d8079261b4cb7894045105330
Ellen N Evans <ellennewellevans@MacPro-EEvans.attlocal.net>
2019-08-25 16:32:46 -0400

    fixed so you can now save comments

--- models/Article.js
+++ models/Article.js
@@ -1,37 +1,41 @@
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// Save a reference to the Schema constructor
-var Schema = mongoose.Schema;
+//Create Schema
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    summary: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    saved: {
+        type: Boolean,
+        default: false
+    },
+    comment: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+        ]
+    });
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
-var ArticleSchema = new Schema({
-  // `title` is required and of type String
-  title: {
-    type: String,
-    required: true
-  },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
-  },
-  date: String,
-  saved: {
-    type: Boolean,
-    default: false
-  },
-  // `comment` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  comment: {
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }
-});
+    //Create model from Schema
+    const Article = mongoose.model('Article', ArticleSchema);
+    
+    module.exports = Article;
 
-// This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
-
-// Export the Article model
-module.exports = Article;
