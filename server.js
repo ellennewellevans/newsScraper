25483a50afdbb0945c0f1c54320a729bc81ebb60
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-08-27 09:13:05 -0400

    updated local db name

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 
 let PORT = process.env.PORT || 3000;
-let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/news_scraper';
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 //Init Express
 const app = express();
@@ -14,12 +14,16 @@ const app = express();
 //Use morgan logger to log all requests
 app.use(logger('dev'));
 //Sets up the express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 //Uses the express.static middleware to serve the static portions of the app
 app.use(express.static("public"));
 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 //Config database
@@ -28,13 +32,12 @@ mongoose.connect(MONGODB_URI);
 
 //Connection Checks
 let db = mongoose.connection;
-db.on('error', (error) =>{
+db.on('error', (error) => {
     console.log('Connection error ${error}');
 });
 require('./routes/routes.js')(app);
 
 //Start Server
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
-
